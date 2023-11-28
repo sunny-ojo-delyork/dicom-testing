3b04b20ec3ac07788bb91b5cbb040f4af687c137
sunny-ojo <sunny@delyorkinternational.com>
2023-11-28 07:17:21 +0100

    auth scaffolding

--- app/Http/Controllers/API/Auth/VerifyEmailController.php
+++ app/Http/Controllers/API/Auth/VerifyEmailController.php
@@ -38,22 +38,5 @@ class VerifyEmailController extends Controller
         ]);
     }
 
-    /**
-     * Mark the authenticated user's email address as verified.
-     *
-     * @param  \Laravel\Fortify\Http\Requests\VerifyEmailRequest  $request
-     * @return \Laravel\Fortify\Contracts\VerifyEmailResponse
-     */
-    // public function __invoke(VerifyEmailRequest $request)
-    // {
-    //     if ($request->user()->hasVerifiedEmail()) {
-    // return app(VerifyEmailResponse::class);
-    //     }
-
-    //     if ($request->user()->markEmailAsVerified()) {
-    //         event(new Verified($request->user()));
-    //     }
-
-    //     return app(VerifyEmailResponse::class);
-    // }
+ 
 }
